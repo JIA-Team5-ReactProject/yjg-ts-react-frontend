d889d5e339f46c6e888205a64b46ce93a3825523
JiHoon <kjhun9062@g.yju.ac.kr>
2024-02-07 13:22:07 +0900

    Feat: マイ情報の参照と情報更新の実装

--- src/components/mypage/PasswordConfirmation.tsx
+++ src/components/mypage/PasswordConfirmation.tsx
@@ -5,7 +5,10 @@ import { passwordValues } from "../../types/auth";
 import { trimValues } from "../../utils/validate";
 import { customAxios } from "../../services/customAxios";
 
-function PasswordConfirmation() {
+function PasswordConfirmation(props: {
+  setPasswordCheck: (value: boolean) => void;
+}) {
+  const { setPasswordCheck } = props;
   const {
     handleSubmit,
     register,
@@ -14,13 +17,14 @@ function PasswordConfirmation() {
   } = useForm<passwordValues>();
   const userData = useRecoilValue(UserDataAtom);
 
+  //내정보 비밀번호체크 제출 함수
   const onSubmit: SubmitHandler<passwordValues> = async (data) => {
     const trimData = trimValues(data);
     try {
       const pwCheckPost = await customAxios.post("/api/admin/verify-password", {
         password: trimData.password,
       });
-      console.log(pwCheckPost);
+      setPasswordCheck(true);
     } catch (error) {
       setError("password", {
         type: "manual",
@@ -29,8 +33,8 @@ function PasswordConfirmation() {
     }
   };
   return (
-    <div className="border border-black h-full w-full text-center">
-      <p className="text-4xl font-bold mt-20">비밀번호확인</p>
+    <div className="border border-black pt-24 pb-6 h-full w-full text-center overflow-y-scroll">
+      <p className="text-4xl font-bold">비밀번호확인</p>
       <div className="mt-24 text-sm">
         <p>
           {<span className="text-blue-600 text-xl">{userData.name}</span>}님의
@@ -58,7 +62,7 @@ function PasswordConfirmation() {
         </div>
         <button
           type="submit"
-          className="rounded-xl mt-40 ml-auto px-7 bg-cyan-600 py-2 text-base font-bold uppercase text-white shadow-md transition-all hover:shadow-lg hover:shadow-black/10 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+          className="rounded-xl mt-20 ml-auto px-7 bg-cyan-600 py-2 text-base font-bold uppercase text-white shadow-md transition-all hover:shadow-lg hover:shadow-black/10 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
           data-ripple-light="true"
         >
           확인
--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import PasswordConfirmation from "../components/mypage/PasswordConfirmation";
+import MypageForm from "../components/mypage/MypageForm";
 
 function Mypage() {
-  const [passwordCheck, setPasswordCheck] = useState(false);
+  const [passwordCheck, setPasswordCheck] = useState(true);
 
   return (
     <div className="flex-auto h-screen pt-32 pr-6 pb-6">
-      {passwordCheck ? null : <PasswordConfirmation />}
+      {passwordCheck ? (
+        <MypageForm />
+      ) : (
+        <PasswordConfirmation setPasswordCheck={setPasswordCheck} />
+      )}
     </div>
   );
 }
