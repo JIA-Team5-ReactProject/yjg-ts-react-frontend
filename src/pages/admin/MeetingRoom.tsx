e6a58e7447a134d9c8b777770b47a619a84bed09
JiHoon <kjhun9062@g.yju.ac.kr>
2024-02-20 18:34:24 +0900

    Feat: 会議室ページの追加

---
+++ src/pages/admin/MeetingRoom.tsx
@@ -0,0 +1,11 @@
+import RoomReservation from "../../components/admin/RoomReservation";
+
+function MeetingRoom() {
+  return (
+    <div className="flex-auto h-screen pt-36 pr-6 pb-6">
+      <RoomReservation />
+    </div>
+  );
+}
+
+export default MeetingRoom;
